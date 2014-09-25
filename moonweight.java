a4ad4eb9f88dd9d8ff921b69ddec954c18b626fe
stef990 <stef990@users.noreply.github.com>
2014-09-25 22:17:01 +0100

    Update moonweight.java

--- moonweight.java
+++ moonweight.java
@@ -5,7 +5,7 @@ public static void main(String args[]){
   double earth_weight = 75*9.81;
   double moon_weight = earth_weight*0.17;
   
-  System.out.println("My weight on the moon is" + moon_weight + "Newtons");
+  System.out.println("My weight on the moon is " + moon_weight + "Newtons");
   
   }
 }
