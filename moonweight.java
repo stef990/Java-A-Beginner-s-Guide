70711214d4bdb90d364bb2e7e868effb0262548a
stef990 <stef990@users.noreply.github.com>
2014-09-25 22:07:15 +0100

    Create moonweight.java

---
+++ moonweight.java
@@ -0,0 +1,12 @@
+/* Program to calculate weight on the moon */
+
+class moonweight{
+public static void main(String args[]){
+  double earth_weight = 75*9.81;
+  double moon_weight = earth_weight*0.17;
+  
+  System.outprintln("My weight on the moon is" + moon_weight + "Newtons");
+  
+  }
+}
+
