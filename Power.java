458e07bca637898b4dd45f296bf149806ba52efa
stef990 <stefanogarombo@gmail.com>
2015-02-05 22:17:08 +0000

    Power class

---
+++ Power.java
@@ -0,0 +1,18 @@
+
+class Power{
+
+public static void main(String arg[]){
+	int e;
+	int result;
+	
+	for(int i = 0;i<10;i++){
+		result = 1;
+		e = i;
+		while (e >0){
+			result *= 2;
+			e--;
+			}
+		System.out.println("2 to the " + i + " power is " + result);
+	}
+}
+}	
