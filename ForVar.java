fc2999356ba9ead5d061fcfe64eec7b721e870bc
stef990 <stefanogarombo@gmail.com>
2015-05-02 00:31:52 +0100

    Chp 3 Example exercise

---
+++ ForVar.java
@@ -0,0 +1,16 @@
+//Declare loop control variable inside the for.
+class ForVar{
+public static void main(String args[]){
+	int sum = 0;
+	int fact = 1;
+	
+	//compute factorial of numbers through 5
+	for(int i =1;i<=5;i++){
+		sum += i;
+		fact *= i;
+		};
+		
+		System.out.println("Sum is " + sum);
+		System.out.println("Factorial is " + fact);
+	}}
+	
---
+++ comma.java
@@ -0,0 +1,10 @@
+//Use a comma in a FOR statement
+class comma{
+	public static void main(String args[]){
+		int i ,j;
+		
+		for (i=0,j=10; i< j ; i++, j--){
+			System.out.println("i and j: " + i + " " + j);
+		}
+	}
+}
