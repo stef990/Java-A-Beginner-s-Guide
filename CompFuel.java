2648acbe9f5f7f1eefc9ab5f93b5970dda7873a7
stef990 <stefanogarombo@gmail.com>
2015-08-31 18:36:20 +0100

    ch 4 pg 118
    parameterized method

---
+++ CompFuel.java
@@ -0,0 +1,48 @@
+/*
+
+ADd a parameterized method that computes the
+fuel required for a given distance
+
+*/
+
+class Vehicle{
+	int passengers; //number of passengers
+	int fuelcap; //capacity in gallons
+	int mpg;
+	//Display the range
+	int range() {
+		return mpg * fuelcap;
+	}
+	//Compute fuel needed for a given distance
+	double fuelneeded(int miles){
+		return (double) miles / mpg;
+	}
+}
+
+//This class declares an object of type vehicle/
+class CompFuel{
+	public static void main(String args[]){
+		Vehicle minivan = new Vehicle();
+		Vehicle sportscar = new Vehicle();
+		double gallons;
+		int dist = 252;
+		
+		//assign values to fields in minivan
+		minivan.passengers = 7;
+		minivan.fuelcap = 16;
+		minivan.mpg = 21;
+		
+		//assign values to fields in sportscar
+		sportscar.passengers = 2;
+		sportscar.fuelcap = 14;
+		sportscar.mpg = 12;
+		
+		gallons = minivan.fuelneeded(dist);
+		System.out.println("To go" + dist + "minivan needs " + gallons);
+		gallons = sportscar.fuelneeded(dist);
+		System.out.println("To go" + dist + "sportscar needs " + gallons);
+		
+	}
+	
+
+}
